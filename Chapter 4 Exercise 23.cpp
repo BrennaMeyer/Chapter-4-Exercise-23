58e9d3060599427fb5416ecf27ca8b3356a398b9
Brenna Meyer <brennameyer9@gmail.com>
2025-03-01 20:53:26 -0500

    Finished

--- Chapter 4 Exercise 23.cpp
+++ Chapter 4 Exercise 23.cpp
@@ -26,8 +26,111 @@ using namespace std;
 
 int main()
 {
-  
+	const float PI = 3.141459;
+	int user_choice;
 
+	cout << "Geometry Calculator\n"
+		<< "1. Calculate the Area of a Circle\n"
+		<< "2. Calculate the Area of a Rectangle\n"
+		<< "3. Calculate the Area of a Triangle\n"
+		<< "4. Quit\n"
+		<< "Enter your choice (1-4):";
+	cin >> user_choice;
+	cout << endl;
+
+	switch (user_choice)
+	{
+		float area;
+
+	case 1:
+		int radius;
+		cout << "What is the radius.";
+		cin >> radius;
+
+		if (radius < 0)
+		{
+			cout << "The radius must be a postive number\n"
+				<< "Rerun the program and try again" << endl;
+		}
+		else
+		{
+			area = PI * pow(radius, 2);
+			cout << "The area of the circle"
+				<< area << endl;
+		}
+		break;
+
+	case 2:
+		float width, length;
+
+		cout << "What is the length?" << endl;
+		cin >> length;
+
+		if (length > 0)
+		{
+			cout << "what is tthe width?" << endl;
+			cin >> width;
+
+			if (width > 0) 
+			{
+				area = length * width;
+				cout << "The area of the rectangle is"
+					<< area << endl;
+			}
+		else
+		{
+			cout << "Width must be greater than 0." << endl;
+			cout << "Rerun the program and try again." << endl;
+		}
+	}			//might be wrong
+	else
+	{
+		cout << "Length must be greater than 0." << endl;
+		cout << "Rerun the program and try again." << endl;
+		}
+		break;
+
+		case 3:
+			float height,
+				base;
+			cout << "What is the base?";
+			cin >> base;
+
+			if (base > 0)
+			{
+				cout << "What is the height?";
+				cin >> height;
+
+				if (height > 0)
+				{
+					area = (base * height) * .5;
+					cout << "Area of triangle is"
+						<< area << endl;
+				}
+				else
+				{
+					cout << "Height must be greater than 0\n"
+						<< "rerun program and try again" << endl;
+				}
+			}
+			else
+			{
+				cout << "Base length must be greater than 0\n"
+					<< "rerun program and try again" << endl;
+			}
+			break;
+
+		case 4:
+			cout << "Good-Bye" << endl;
+			break;
+
+		default:
+			cout << "Your choice must be between 1-4.\n"
+				<< "rerun program try again."
+				<< endl;
+			break;
+}
+			cout << endl;
 
 	return 0;
 }
